e4098cce42a6c6c3f236851951e5b211d7fd2f92
mogajiaishah <mogajiaisha0@gmail.com>
2023-09-27 10:02:42 -0700

    update

---
+++ app.js
@@ -0,0 +1,230 @@
+// // console.log("Hello world");
+
+// const fruitname = "orange";
+// console.log(fruitname)
+
+// let y = 50;
+// console.log(y)
+
+// let a = 3;
+// let b = 10
+// let c = a + b
+// console.log(a + b)
+
+// var z = a + b
+// console.log(z)
+
+
+// const firstname = "David", lastname = "Johnson", age = 41;
+// console.log(firstname,lastname,age)
+
+
+// let x = 5;
+// let y = 7;
+// var z = x * y
+// console.log(z)
+
+// var a  = 13;
+// a /= 7;
+// console.log(a)
+
+// const b = 10;
+// const c = 10 + 10
+// console.log(c)
+
+// let d = 10;
+// d *= 3;
+// console.log(d)
+
+// 1-number
+// 2-stringdatatype
+// 3-objects 
+
+// const x = 10;
+// const y = 5
+// console.log(x > y)
+
+// let a = 24
+// let b = 24
+// console.log(a == b)
+
+// const c = 7
+// const d = 3
+// console.log(c != d)
+
+// const age = 18;
+// const convertedAge = String(age);
+// const number = prompt('Enter Your age')
+// if(number < convertedAge){
+//     alert('you are too young for this')
+// }
+//  else if(number > convertedAge){
+//     alert("you are too old for this")
+// }
+// else if(number === convertedAge){
+//     alert("you are cool")
+// }
+
+// var x =15
+// var y = 8 
+// if(x > y){
+//     alert("Hello world")
+// } 
+
+// let text = "i eat apples the whole day"
+// let textSlice = text.slice(6,-13 )
+// console.log(textSlice)
+
+// let txt ="dsakdhdas"
+// let index = txt.indexOf("h")
+// console.log(index)
+
+
+// const word = "Hello world"
+// const newWord = word.replace("Hello", "Hi")
+// console.log(newWord)
+
+// const greeting = "hello world"
+// const greetingUppercase = greeting.toUpperCase();
+// console.log(greetingUppercase)
+
+// const greetingLowercase = greetingUppercase.toLowerCase();
+// console.log(greetingLowercase)
+
+
+let fruits = ["Orange", "Apple", "Banana"];
+
+
+
+// let fruits = ["Banana", "Apple", "Mango"];
+// fruits[0] = "Orange"
+// console.log(fruits)
+
+// let fruits = ["Orange", "Apple", "Mango"];
+// console.log(fruits.length)
+
+
+
+
+// let markHeight = '1.69m', mass = '78kg';
+// console.log(markHeight, mass)
+
+// let johnMass = '92kg', height ='1.95m';
+// console.log(johnMass, height)
+
+
+// let markHeight = 1.69
+// let markMass = 78
+// let markBmi = markMass/markHeight*markHeight
+// console.log(markBmi)
+
+
+// let johnMass =92
+// let johnHeight = 1.95
+// let johnBmi = johnMass/johnHeight*johnHeight
+// console.log(johnBmi)
+
+
+
+// let markMass = '95kg', height = '1.88m';
+// console.log(markMass, height)
+
+// let johnHeight = '1.76m', mass = '85kg'
+// console.log(johnHeight, mass)
+
+
+// let markMass = 95
+// let markHeight = 1.88
+// let markBmi = markMass/markHeight*markHeight
+// console.log(markBmi)
+
+// let johnMass = 85
+// let johnHeight = 1.76
+// let johnBmi = johnMass/johnHeight*johnHeight
+// console.log(johnBmi)
+
+// if(johnBmi < markBmi){
+//     alert("true")
+// }
+// else{markBmi > johnBmi} {
+//     alert("false")
+// }
+
+
+// const arrayNumber = [20, 21, 18, 15, 10, 9, 8, 7, 45,];
+// let lowestElement = arrayNumber[0];
+// for (let i = 0; i > arrayNumber.length; i++){
+//     if (arrayNumber[i] < lowestElement){
+//         lowestElement = arrayNumber[i]
+//     }
+// }
+// console.log(lowestElement)
+
+// function getFiveMultiples() {
+//     for (let i = 0; i < 100; i++) {
+//         //         if (i % 5 === 0) {
+//             // console.log(i);
+//         }
+//     }
+// }
+getFiveMultiples();
+
+
+function getThreeMultiples() {
+for (let i = 0; i < 100; i++) {
+    if (i % 3 === 0) {
+       console.log(i)}
+            
+}
+}
+
+getThreeMultiples();
+// console.log(i)
+ 
+// let vegetables = ["Tomato", "Carrot", "Broccoli"]
+// vegetables.pop()
+// console.log(vegetables)
+
+// let vegetables = ["Tomato", "Carrot", "Garlic"];
+// vegetables.push('Broccoli');
+// console.log(vegetables)
+
+// let vegetables = ["Garlic", "Carrot", "Tomato", "Broccoli"];
+// vegetables.splice(1, 2)
+// console.log(vegetables)
+
+// let vegetables = ["Garlic", "Carrot", "Broccoli", "Pumpkin"];
+//  vegetables.sort()
+// console.log(vegetables)
+
+
+// function sortAlphabets(){
+//     const alphabets = ['paranoid']
+//     const alphabetsSorted = alphabets.sort(function(a, b){
+//         return a - b
+//     }
+//     );
+//     console.log(alphabetsSorted)
+// }
+// sortAlphabets()
+
+// const loopNumber = (a) =>{
+//     console.log(a * a)
+// }
+// loopNumber(24,44,68,96,32.10)
+
+// function loopNumber(){
+//     const number = [24,44,68,96,32.10]
+// } 
+
+// callback function
+// function sortNumbers(){
+// const number = [1, 3,5,7,9,2, 4, 6, 8, 10, 21, 23]
+// const numberSorted = number.sort(function(aishah, mogaji){
+//     return mogaji - aishah
+// });
+// console.log(numberSorted)
+// }
+
+// sortNumbers()
+const number = [1,2,3,4,5]
